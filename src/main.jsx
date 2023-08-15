a2468a3fca60d0e45617963adb03255888fee668
Narayan Aryal <aryal.coding@gmail.com>
2023-08-15 22:58:08 +1000

    reuseable form created

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { api } from "./state/api.js";
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
-  middleware: (getDefault) => getDefault.concat(api.middleware),
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
 
 setupListeners(store.dispatch);
